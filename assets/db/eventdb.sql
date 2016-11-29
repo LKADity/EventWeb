d1dcf53cfd18bf7e633e27b3c733dee38345390b
chaosfrezz\LKADity <avinek76@gmail.com>
2016-11-29 17:56:02 +0700

    Database

---
+++ assets/db/eventdb.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 29, 2016 at 11:54 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eventdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `performer`
+--
+
+CREATE TABLE `performer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `desciption` text NOT NULL,
+  `pic1` varchar(255) NOT NULL,
+  `pic2` varchar(255) NOT NULL,
+  `pic3` varchar(255) NOT NULL,
+  `pic4` varchar(255) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stand`
+--
+
+CREATE TABLE `stand` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `desciption` text NOT NULL,
+  `pic1` varchar(30) NOT NULL,
+  `pic2` varchar(30) NOT NULL,
+  `owner` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticketing`
+--
+
+CREATE TABLE `ticketing` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(16) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(2) NOT NULL,
+  `date` date NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `ktp` varchar(20) NOT NULL,
+  `contact` varchar(15) NOT NULL,
+  `address` text NOT NULL,
+  `poss` varchar(5) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `performer`
+--
+ALTER TABLE `performer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stand`
+--
+ALTER TABLE `stand`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ticketing`
+--
+ALTER TABLE `ticketing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `performer`
+--
+ALTER TABLE `performer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `stand`
+--
+ALTER TABLE `stand`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ticketing`
+--
+ALTER TABLE `ticketing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

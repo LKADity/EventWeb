82af24db871d7167e94ed088745403ee2152254b
korobuckle <sangpa0@gmail.com>
2016-12-07 06:36:19 +0700

    contact + lineup

---
+++ application/views/contact.php
@@ -0,0 +1,39 @@
+<head>
+	<title>CONTACT</title>
+	<style>
+		.img-header {
+			background: url("<?php echo base_url().'assets/img/bg.jpg'; ?>");
+			background-size: 100%;
+			color: white;
+			height: 150px;
+			margin-bottom: 20px;
+		}
+		.img-header>h1 {
+			line-height: 100px;
+			font-weight: bolder;
+			font-size: 5em;
+			margin-left: 5%;
+		}
+	</style>
+</head>
+<body>
+	<div class="img-header container-fluid">
+		<h1>CONTACT</h1>
+	</div>
+
+	<center style="font-size: 1.2em">
+	<h2><b>ANU PRODUCTION</b></h2>
+	<p>fX Sudirman Lt. 4</p>
+	<p>Jl. Jend Sudirman Pintu Satu Senayan</p>
+	<p>Jakarta Selatan</p>
+	<p>Tel: +6221-7002020</p>
+	<p>Email: info@anu.co.id</p>
+	</center>
+
+	<center><iframe src="https://maps.google.com/maps?q=fx sudirman, Jakarta&t=&z=14&ie=UTF8&iwloc=B&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" height="480" width="70%" style="margin-top: 30px; margin-bottom: 30px">
+	<a class="google-map-code" href="http://www.embed-google-map.com" id="get-map-data">embed-google-map.com</a>
+	</iframe></center>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
---
+++ application/views/lineup.php
@@ -0,0 +1,71 @@
+<head>
+	<title>LINEUP</title>
+	<style>
+		.img-header {
+			background: url("<?php echo base_url().'assets/img/bg.jpg'; ?>");
+			background-size: 100%;
+			color: white;
+			height: 150px;
+			margin-bottom: 20px;
+		}
+		.img-header>h1 {
+			line-height: 100px;
+			font-weight: bolder;
+			font-size: 5em;
+			margin-left: 5%;
+		}
+		.square {
+			background-color: #3884c7;
+			height: 330px;
+			width: 95%;
+			margin: 10px;
+			margin-bottom: 30px;
+		}
+		.square>.img-thumbnail {
+			width: 90%;
+			height: 250px;
+			margin-left: 5%;
+			margin-top: 25px;
+		}
+	</style>
+</head>
+<body>
+	<div class="img-header container-fluid">
+		<h1>LINEUP</h1>
+	</div>
+
+	<div class="container">
+		<!--start iteration-->
+		<div class="col-sm-6">
+			<div class="square">
+				<img src="<?php echo base_url().'assets/img/facebook.png'; ?>" class="img-thumbnail">
+				<center><p style="color: white; font-size: 2.5em"><b>LALA</b></p></center>
+			</div>
+		</div>
+
+		<div class="col-sm-6">
+			<div class="square">
+				<img src="<?php echo base_url().'assets/img/facebook.png'; ?>" class="img-thumbnail">
+				<center><p style="color: white; font-size: 2.5em"><b>LALA</b></p></center>
+			</div>
+		</div>
+
+		<div class="col-sm-6">
+			<div class="square">
+				<img src="<?php echo base_url().'assets/img/facebook.png'; ?>" class="img-thumbnail">
+				<center><p style="color: white; font-size: 2.5em"><b>LALA</b></p></center>
+			</div>
+		</div>
+
+		<div class="col-sm-6">
+			<div class="square">
+				<img src="<?php echo base_url().'assets/img/facebook.png'; ?>" class="img-thumbnail">
+				<center><p style="color: white; font-size: 2.5em"><b>LALA</b></p></center>
+			</div>
+		</div>
+		<!--end iteration-->
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>

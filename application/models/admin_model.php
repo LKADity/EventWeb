dc1899ef8ed88bb344d937aec94a005edaa269f4
atepagung <agungteja64@gmail.com>
2016-12-07 09:00:11 +0700

    admin controller update

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -35,6 +35,76 @@ class admin_model extends CI_Model {
 		return $query;
 	}
 
+	public function select_ticket() {
+		$condition = 'status <> 2';
+		$query = $this->db
+				->select('id, username, amount, status')
+				->from('ticket')
+				->where($condition)
+				->get();
+
+		return $query;
+	}
+
+	public function acc_performer($id) {
+		$data['status'] = '1';
+		$query = $this->db
+			->where('id', $id);
+			->update('performer', $data);
+
+		return $query;
+	}
+
+	public function acc_stand($id) {
+		$data['status'] = '1';
+		$query = $this->db
+			->where('id', $id);
+			->update('stand', $data);
+
+		return $query;
+	}
+
+	public function acc_ticket($id) {
+		$data['status'] = '1';
+		$query = $this->db
+			->where('id', $id);
+			->update('ticket', $data);
+
+		return $query;
+	}
+
+	public function dec_performer($id) {
+		$data['status'] = '2';
+		$query = $this->db
+			->where('id', $id);
+			->update('performer', $data);
+
+		return $query;
+	}
+
+	public function dec_stand($id) {
+		$data['status'] = '1';
+		$query = $this->db
+			->where('id', $id);
+			->update('stand', $data);
+
+		return $query;
+	}
+
+	public function dec_ticket($id) {
+		$data['status'] = '1';
+		$query = $this->db
+			->where('id', $id);
+			->update('ticket', $data);
+
+		return $query;
+	}
+
+	public function del_user($id) {
+		$query = $this->db->delete('user', array('id' => $id));
+
+		return $query;
+	}
 
 }
  ?>
